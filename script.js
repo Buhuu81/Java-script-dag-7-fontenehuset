c83689980465021836dbe1e5cf199bc6db175c09
Buhuu81 <tommyjohansen1981@gmail.com>
2025-05-15 12:31:50 +0200

    Dag 8

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 // console referer til inspiser vinduet .log gir en beskjed til console
-console.log('Hello World');
+console.log("Hello World!");
 
-//Datatyper:
+// Datatyper:
 // string = streng/tekst
 // number = tall
-// boolean = true/false
+// boolean = true/false sant/usant verdi
 
-//variabler:
+// Variabler:
 // let = kan bytte verdi
-// cons = ikke bytte verdi
+// const = ikke bytte verdi
 
-// erklærer en variabel:
-// let = ? = ?;
-let username = 'Tommy';
-console.log(username);
-username = 'Knut';
-console.log(username);
+// Erklærer en variabel:
+// let ? = ?;
+let userName = "Julie";
+console.log(userName);
+userName = "Julius";
+console.log(userName);
 
-let userAge = 43;
+let userAge = 28;
 console.log(userAge);
 
 let userPresent = true;
 console.log(userPresent);
 
-const firstName = 'Tommy';
-const lastName = 'Johansen';
-const fullName = firstName + ' ' + lastName;
-const wholeName = ``
+const firstName = "Julie";
+const lastName = "Hauge";
+const fullName = firstName + " " + lastName + "!"; // Variable chaining
+const wholeName = `${firstName} ${lastName}!`; // Template litteral
 console.log(fullName);
+console.log(wholeName);
+
+function businessCard() {
+    const firstName = "Tommy";
+    console.log(firstName);
+    // Sett opp en variabel for lastName
+    const lastName = "Johansen";
+    // Sett opp en variabel for fullName
+    const fullName = firstName + " " + lastName;
+    // Logg fullName til console
+    console.log(fullName);
+
+
+    // Mer om dette tirsdag 19.05
+    const body = document.querySelector("h1");
+    body.textContent = fullName;
+}
+businessCard();
