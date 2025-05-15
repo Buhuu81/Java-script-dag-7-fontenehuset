dd5468fc6c4383a6873c48caffd439223c8d19c3
Buhuu81 <Tommyjohansen1981@gmail.com>
2025-05-15 11:20:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java-script-dag-7-fontenehuset
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Buhuu81/Java-script-dag-7-fontenehuset)
---
+++ script.js
@@ -0,0 +1,30 @@
+// console referer til inspiser vinduet .log gir en beskjed til console
+console.log('Hello World');
+
+//Datatyper:
+// string = streng/tekst
+// number = tall
+// boolean = true/false
+
+//variabler:
+// let = kan bytte verdi
+// cons = ikke bytte verdi
+
+// erklærer en variabel:
+// let = ? = ?;
+let username = 'Tommy';
+console.log(username);
+username = 'Knut';
+console.log(username);
+
+let userAge = 43;
+console.log(userAge);
+
+let userPresent = true;
+console.log(userPresent);
+
+const firstName = 'Tommy';
+const lastName = 'Johansen';
+const fullName = firstName + ' ' + lastName;
+const wholeName = ``
+console.log(fullName);
